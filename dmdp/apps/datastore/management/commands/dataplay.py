7deb6917e67fe50d27b2d4f09032b4a62ddf9403
Vlada Macek <macek@sandbox.cz>
2017-01-04 00:43:29 +0100

    Extended playing with partitions. Added multiple inserts. Added lookup caching example.

--- dmdp/apps/datastore/management/commands/dataplay.py
+++ dmdp/apps/datastore/management/commands/dataplay.py
@@ -1,37 +1,21 @@
+from collections import defaultdict
+from datetime import timedelta
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.db.transaction import rollback, atomic
+from django.db.transaction import atomic
+from django.utils import timezone
 
 from dmdp.apps.datastore.models import Event, Browser
-
-
-CurBrowser = Browser.YM()
-CurEvent = Event.YM()
 
 
 class Command(BaseCommand):
     help = "Plays with partitions a bit."
 
-    def out(self, msg):
-        self.stdout.write(self.style.NOTICE(msg))
-
-    def play_simple(self):
-        browser = CurBrowser.objects.create(
-            ua="That-Mozilla",
+    def out(self, msg=None):
+        self.stdout.write(
+            self.style.SUCCESS(msg) if msg else self.style.NOTICE('------------------------')
         )
-        self.out("%s record: %r" % (CurBrowser._meta.object_name, browser))
-
-        event = CurEvent.objects.create(
-            browser=browser,
-        )
-
-        del browser
-
-        event = CurEvent.objects.get(id=event.id)
-        self.out("event.browser = %r" % event.browser)
-
-        browser = CurBrowser.objects.get(ua="That-Mozilla")
-        self.out('"That-Mozilla".event_set.all() = %r' % browser.event_set.all())
 
     @atomic
     def handle(self, *args, **options):
@@ -39,8 +23,82 @@ class Command(BaseCommand):
         settings.DEBUG = True
 
         self.play_simple()
-
-
+        self.play_many()
 
         # rolls back the database
         raise SystemExit
+
+    def play_simple(self):
+        CurBrowser = Browser.YM()
+        CurEvent = Event.YM()
+
+        browser = CurBrowser.objects.create(
+            ua="That-Mozilla",
+        )
+        self.out("%s record: %r" % (CurBrowser._meta.object_name, browser))
+
+        event = CurEvent.objects.create(
+            timestamp=timezone.now(),
+            browser=browser,
+        )
+
+        del browser
+        self.out()
+
+        event = CurEvent.objects.get(id=event.id)
+        self.out("event.browser = %r" % event.browser)
+
+        browser = CurBrowser.objects.get(ua="That-Mozilla")
+        self.out('"That-Mozilla".event_set.all() = %r' % browser.event_set.all())
+
+        self.out()
+
+    def iter_last_days(self, number_of_days):
+        day = timezone.now()
+        delta = timedelta(days=1)
+        for _ in xrange(number_of_days):
+            yield day
+            day -= delta
+
+    def play_many(self):
+        """
+        Create events for the most recent 50 days pointing to the same browser.
+        """
+
+        # Individually
+
+        for day in self.iter_last_days(50):
+            browser_id = Browser.YM(day).get_or_create_cached_pk_for('Wget')
+
+            Event.YM(day).objects.create(
+                timestamp=day,
+                browser_id=browser_id,
+            )
+
+        self.out()
+
+        # In bulk
+
+        bulks = defaultdict(list)
+
+        for day in self.iter_last_days(50):
+            browser_id = Browser.YM(day).get_or_create_cached_pk_for('Curl')
+
+            EventYM = Event.YM(day)
+            bulks[EventYM].append(
+                EventYM(
+                    timestamp=day,
+                    browser_id=browser_id,
+                )
+            )
+
+        for model, items in bulks.items():
+            model.objects.bulk_create(items)
+
+        self.out()
+
+        for BrowserYM, EventYM in zip(Browser.iter_YMs(), Event.iter_YMs()):
+            self.out('%s - %s items' % (BrowserYM._meta.object_name, BrowserYM.objects.count()))
+            self.out('%s - %s items' % (EventYM._meta.object_name, EventYM.objects.count()))
+
+        self.out()
--- dmdp/apps/datastore/models.py
+++ dmdp/apps/datastore/models.py
@@ -1,12 +1,17 @@
 import datetime
+import hashlib
 
 from django.db import models
+from django.core.cache import cache
 
 from . import partitions
 
 
 @partitions.make_model_monthly_partitioned(globals())
 class Browser(models.Model):
+    """
+    A table for lookups. (referenced)
+    """
     ua = models.CharField(max_length=100, unique=True)
     some_value = models.IntegerField(default=0)
 
@@ -15,6 +20,24 @@ class Browser(models.Model):
 
     def __unicode__(self):
         return self.ua
+
+    @classmethod
+    def get_or_create_cached_pk_for(cls, ua):
+        """
+        Returns the cached primary key for unique object value.
+        """
+        key = 'idcache-%s-%s' % (
+            # a string like 'Browser_2016_11'
+            cls._meta.object_name,
+            # a hash, because the remote cache server may not support all characters in raw string as key
+            hashlib.sha256(ua.encode('UTF-8')).hexdigest(),
+        )
+        pk = cache.get(key)
+        if pk is None:
+            pk = cls.objects.get_or_create(ua=ua)[0].pk
+            cache.set(key, pk)
+
+        return pk
 
 
 @partitions.make_model_monthly_partitioned(globals())
